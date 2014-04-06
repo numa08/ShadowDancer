3ab28a21ef661ac61cd2a15740861f9da2fd6cbf
numa08 <n511287@gmail.com>
2014-04-06 11:01:36 +0900

    TwitterEvalの利用
    Twitter Evalを利用してDSLのパースを行う

--- src/shadowdancer/src/main/scala/App.scala
+++ src/shadowdancer/src/main/scala/App.scala
@@ -1,5 +1,12 @@
 package net.numa08
 
+import java.io.File
+
+import util.FileWrapper._
+import scala.io.Source
+import com.twitter.util.Eval
+import net.numa08.dsl.DancerText
+
 /** The launched conscript entry point */
 class App extends xsbti.AppMain {
   def run(config: xsbti.AppConfiguration) = {
@@ -8,11 +15,22 @@ class App extends xsbti.AppMain {
 }
 
 object App {
-  /** Shared by the launched version and the runnable version,
-   * returns the process status code */
+  /**
+   * Shared by the launched version and the runnable version,
+   * returns the process status code
+   */
   def run(args: Array[String]): Int = {
-    println("Hello World: " + args.mkString(" "))
-    0
+    val retval = new File("DancerText")
+      .asOpt
+      .map { f =>
+        val code = Source.fromFile(f).mkString("import net.numa08.dsl._\n", "", "")
+        new Eval()[DancerText](code)
+        0
+      }.getOrElse {
+        sys.error("DancerText is not exist")
+        1
+      }
+    retval
   }
   /** Standard runnable class entrypoint */
   def main(args: Array[String]) {
