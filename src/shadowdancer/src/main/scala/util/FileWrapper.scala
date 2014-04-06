f21f23231280bdfa035a5932674140193f47089b
numa08 <n511287@gmail.com>
2014-04-06 10:04:06 +0900

    FileをOptionとして扱う暗黙的型変換の実装
    Fileが存在する場合はSome、存在しない場合はNoneとなるasOptを実装

---
+++ src/shadowdancer/src/main/scala/util/FileWrapper.scala
@@ -0,0 +1,10 @@
+package util
+
+import java.io.File
+
+object FileWrapper {
+  implicit class FileWrapper(val file : File) extends AnyVal {
+    def asOpt : Option[File] = Option(file).filter(_.exists())
+  }
+
+}
