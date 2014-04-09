fe3b4a93f2b871373a85d6a3bcee7b5f9fef957e
numa08 <n511287@gmail.com>
2014-04-09 23:57:37 +0900

    Define kagemai project url
    define kagemai project url on dsl.
    fixes #9

--- src/shadowdancer/src/test/scala/net/numa08/dsl/DancerTextTest.scala
+++ src/shadowdancer/src/test/scala/net/numa08/dsl/DancerTextTest.scala
@@ -7,9 +7,10 @@ import net.numa08.dsl.BtsServices._
 class DancerTextTest extends FunSuite {
 
   test("Create Project Service") {
-    val projectService = "dir" as Gitlab at "project"
+    val projectService = "dir" as Gitlab at "project" on "http://kagemai.example.com/html/guest.cgi?project=Shadow_dancer"
     assert(projectService.dir.getPath == "dir")
     assert(projectService.service == Gitlab)
     assert(projectService.project == "project")
+    assert(projectService.url == "http://kagemai.example.com/html/guest.cgi?project=Shadow_dancer")
   }
 }
