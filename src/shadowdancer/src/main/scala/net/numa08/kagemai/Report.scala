287cadce605e643fa7552025b4251b94be7f9809
numa08 <n511287@gmail.com>
2014-04-14 00:38:42 +0900

    Upload only active Reports
    active report status(new, accepted, reservation) are uploaded
    other is not uploaded
    fix #19

--- src/shadowdancer/src/main/scala/net/numa08/kagemai/Report.scala
+++ src/shadowdancer/src/main/scala/net/numa08/kagemai/Report.scala
@@ -2,7 +2,7 @@ package net.numa08.kagemai
 
 import scala.xml.{ Text, Elem }
 
-case class Report(id: Int, email: String, title: String, body: String)
+case class Report(id: Int, email: String, title: String, body: String, status: Report.Status.Status)
 
 object Report {
 
@@ -13,7 +13,7 @@ object Report {
       case e: Exception => Option(-1)
     }
 
-    val values = List("email", "title", "body").map { id =>
+    val values = List("email", "title", "body", "status").map { id =>
       val value = (elem \\ "message" headOption).map(_ \ "element").map(_ filter (_ \ "@id" contains (Text(id)))).map(_.headOption.map(_.text).getOrElse("")).getOrElse("")
       (id, value)
     }.toMap
@@ -23,8 +23,32 @@ object Report {
       email <- values.get("email")
       title <- values.get("title")
       body <- values.get("body")
+      statusName <- values.get("status")
     } yield {
-      Report(id, email, title, body)
+      Report(id, email, title, body, Status.withJAName(statusName))
     }).get
+  }
+
+  object Status extends Enumeration {
+    type Status = Value
+    val New, Accepted, Reservation, Fixed, Deferred, Resolved = Value
+
+    private val jaNames = Map("新規" -> New, "受付済み" -> Accepted, "保留" -> Reservation, "修正済み" -> Fixed, "却下" -> Deferred, "完了" -> Resolved)
+
+    def withJAName(name: String): Status = {
+      val status = jaNames.get(name)
+      status match {
+        case None => throw new NoSuchElementException(s"${name} is not Status")
+        case _    => status.get
+      }
+    }
+
+    def active: ValueSet = {
+      ValueSet(New, Accepted, Reservation)
+    }
+
+    def inactive: ValueSet = {
+      ValueSet(Fixed, Deferred, Resolved)
+    }
   }
 }
